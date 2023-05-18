857ef44404cca96952f318c9ec676773eccb8bd6
mohan.g <mohan.g@perpetualblock.io.com>
2023-05-18 11:28:35 +0530

    its polygon git repo code

---
+++ test/test.data.ts
@@ -0,0 +1,28 @@
+export const testDid = "0x1bF12849c299867aAbaB8412757eb19C69950C92";
+
+export const updateDidDocument = "Hello Mohan G";
+
+export const privateKey =
+  "0x378f4723dc8c43e85710e56394d00cc071ee7d2c4ad20148440e2688979bf109";
+
+export const url = "https://rpc-mumbai.maticvigil.com/";
+
+export const contractAddress = "0x8B335A167DA81CCef19C53eE629cf2F6291F2255";
+
+export const network = "testnet";
+
+// Job Description
+// • Must be able to demonstrate channel creation, lifecycle chain code management, adding an organization to an existing network, using fabric ca for generating the certificates
+// • Mandatory Skills: Kubernetes, Docker, Ansible, Terraform, AWS
+// • Must be able to demonstrate upgrades to an existing Hyperledger fabric network
+// • Must be able to perform certificate renewals/updates for an existing Hyperledger fabric network
+// • Continuous Integration and automation skills including GitHub Actions
+// • Being a Certified Hyperledger Fabric Administrator would be an added advantage
+
+// The Successful Applicant
+// • Deploy Fabric applications on Kubernetes
+// • Monitoring Kubernetes system
+// • Implement and improve monitoring and alerting
+// • Build and maintain highly available blockchain systems on Kubernetes
+// • Implement an auto-scaling system for our Kubernetes nodes
+// • Linux Administration
